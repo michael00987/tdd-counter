372ef20862a5c22018ec593c03ea5912e428c4fb
michael00987 <hyeongkwangseok@gmail.com>
2020-09-20 21:46:34 +0900

    Text

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [counter, setCounter] = useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>This is counter app</h1>
+      <div id='counter-value'>{counter}</div>
+      <button id='increment-btn'>increment</button>
+      <button id='decrement-btn'>increment</button>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,32 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import {render} from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import {configure, shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({adapter: new Adapter()});
+
+describe('Counter Testing', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<App />);
+  });
+
+  test('render the title of counter', () => {
+    expect(wrapper.find('h1').text()).toContain('This is counter app');
+
+    // const {getByText} = render(<App />);
+    // const linkElement = getByText('This is counter app');
+    // expect(linkElement).toBeInTheDocument();
+  });
+
+  test('render a button with text of `increment`', () => {
+    expect(wrapper.find('#increment-btn').text()).toBe('increment');
+  });
+
+  test('render the initial value of state in a div`', () => {
+    expect(+wrapper.find('#counter-value').text()).toBe(0);
+  });
 });
